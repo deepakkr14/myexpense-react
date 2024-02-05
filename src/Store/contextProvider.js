71f0c1d96e1f684ebcef997619ad7ceba7ddf000
deepakkr14 <deepakbipin142@gmail.com>
2024-02-05 20:42:57 +0530

    modified some  code and added context

---
+++ src/Store/contextProvider.js
@@ -0,0 +1,45 @@
+const userIsLoggedIn = !!token;
+const [isLoggedIn, setIsLoggedIn] = useState(false);
+const [items,updateItems]=useState([]) 
+
+
+const loginHandler = (token) => {
+  setToken(token);
+  localStorage.setItem('ExpenseToken',token)
+};
+
+const logoutHandler = () => {
+  setToken(null);
+  localStorage.removeItem('ExpenseToken')
+};
+
+const addItemToCartHandler=item=>{
+  updateItems([...items,item])             
+  // postCartData(item);
+}
+
+const removeItemFromHandler=id=>{
+// alert(id)   
+const updatedItems=items.filter((item)=>item.id!==id)
+updateItems(updatedItems)     
+// removeItem(id)
+}
+
+
+const contextValue = {
+  token: token,
+  logout: logoutHandler,
+   items:items,
+   addItem:addItemToCartHandler,
+    removeItem:removeItemFromHandler,
+
+};
+
+return (
+  <ExpenseContext.Provider value={contextValue}>
+    {props.children}
+  </ExpenseContext.Provider>
+);
+
+
+export default ExpenseContextProvider;
--- src/pages/ProfileUpdate.js
+++ src/pages/ProfileUpdate.js
@@ -4,9 +4,12 @@ import { Button, Form, Row, Col } from "react-bootstrap";
 
 const ProfileUpdate = (props) => {
   const [Email, setEmail] = useState("");
-  const newName = useRef();
-  const newPhoto = useRef();
-  const imageRef = useRef();
+  const [emailStatus, Verified] = useState("");
+  const [newName, setName] = useState("");
+  const [newPhoto, setPhoto] = useState("");
+  const [imageRef,setUrl] = useState("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png");
+  const nameChangeHandler = (event) => setName(event.target.value);
+ const photoChangeHandler = (event) => setPhoto(event.target.value);
   useEffect(() => {
     fetch(
       "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyCAddUzy56S_Fd9ynLhR2NrwXQPUB1M2i8",
@@ -22,64 +25,24 @@ const ProfileUpdate = (props) => {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-        newPhoto.current.value = data.users[0].photoUrl || "";
-        newName.current.value = data.users[0].displayName || "";
-        imageRef.current.src =
-          data.users[0].photoUrl ||
-          "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
-        setEmail((prev)=>data.users[0].email);
+        console.log(data)
+        setName(data.users[0].displayName);
+        setPhoto(data.users[0].photoUrl);
+        setUrl(data.users[0].photoUrl || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png");
+        setEmail(data.users[0].email);
+        Verified(data.users[0].emailVerified);
       });
-    // .catch((error)=>console.log(error))
   }, [Email]);
-  const verifyEmail = async () => {
-    fetch(
-      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyCAddUzy56S_Fd9ynLhR2NrwXQPUB1M2i8",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          idToken: localStorage.getItem("token"),
-          requestType: "VERIFY_EMAIL",
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    )
-      .then((res) => {
-        if (res.ok) {
-          return res.json().then((data) => {
-            console.log(data);
-            alert(`user verified succesfully`);
-          });
-        } else {
-          return res.json().then((data) => {
-            let errorMessage = "Authentication failed!";
-            if (data && data.error && data.error.message) {
-              errorMessage = data.error.message;
-            }
-            throw new Error(errorMessage);
-          });
-        }
-      })
-
-      .catch((err) => {
-        alert(err.message);
-      });
-  };
   const submitUpdate = async (e) => {
     e.preventDefault();
-    const updatedName = newName.current.value;
-    const updatedPhoto = newPhoto.current.value;
-    console.log("submitted", updatedName, updatedPhoto);
     await fetch(
       "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCAddUzy56S_Fd9ynLhR2NrwXQPUB1M2i8",
       {
         method: "POST",
         body: JSON.stringify({
           idToken: localStorage.getItem("token"),
-          displayName: updatedName,
-          photoUrl: updatedPhoto,
+          displayName: newName,
+          photoUrl: newPhoto,
           returnSecureToken: true,
         }),
         headers: {
@@ -91,7 +54,7 @@ const ProfileUpdate = (props) => {
         if (res.ok) {
           return res.json().then((data) => {
             alert(`user details has succesfully  updated`);
-            setEmail(data.users[0].email);
+            setEmail(data.email);
           });
         } else {
           return res.json().then((data) => {
@@ -106,6 +69,7 @@ const ProfileUpdate = (props) => {
       })
 
       .catch((err) => {
+        console.log('submit update',err)
         alert(err.message);
       });
   };
@@ -113,14 +77,14 @@ const ProfileUpdate = (props) => {
     <Fragment>
       <Form className="pform ">
         <Row className=" m-1 text-center justify-content-center">
-          <h2>My Profile</h2> 
+          <h2>My Profile</h2>
           <h3>Registered Email :{Email}</h3>
 
           <Col xs={2} className="me-3">
             <img
               style={{ width: "110px", height: "120px" }}
-              src=""
-              ref={imageRef}
+              src={imageRef}
+              
               alt="display"
             />
           </Col>
@@ -132,7 +96,8 @@ const ProfileUpdate = (props) => {
               size="md"
               type="text"
               placeholder="Full Name"
-              ref={newName}
+              value={newName}
+              onChange={nameChangeHandler}
             />
           </Col>
           <Col xs={2}>
@@ -143,7 +108,8 @@ const ProfileUpdate = (props) => {
               size="md"
               type="url"
               placeholder="Profile Photo URL"
-              ref={newPhoto}
+              onChange={photoChangeHandler}
+              value={newPhoto}
             />
           </Col>
           <Row className="mt-4 d-flex justify-content-end">
@@ -154,12 +120,12 @@ const ProfileUpdate = (props) => {
             </Col>
             <Col md={4}>
               <Button
-                className={`Ebtn ${!Email ? "on" : "off"}`}
+                className={`Ebtn ${!emailStatus ? "on" : "off"}`}
                 variant="outline-primary"
                 onClick={verifyEmail}
-                disabled={Email}
+                disabled={emailStatus}
               >
-                {Email ? "Email Verified" : "Verify Email"}
+                {emailStatus ? "Email Verified" : "Verify Email"}
               </Button>
             </Col>
             <Col md={4}>
